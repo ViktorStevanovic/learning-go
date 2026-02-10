45c2ae42d2fb630ee6f37de8d476c19aa50774c4
viktare <stevanovic.viktor@gmail.com>
2026-02-10 13:33:34 +0100

    helloworld with tests

--- hello-go/hello.go
+++ hello-go/hello.go
@@ -2,10 +2,35 @@ package main
 
 import "fmt"
 
-func Hello() string {
-	return "Hello, world"
+const (
+	spanish = "Spanish"
+	french  = "French"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetinPrefix(language) + name
+}
+
+func greetinPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(Hello("Viktor", ""))
 }
